842f0a9b4cdc00fc68c6f894e16edc0a21aca435
PattharananZ <42967288+PattharananZ@users.noreply.github.com>
2018-11-30 01:39:38 +0700

    PlottingWebPygalAndMatplotlib

--- graph1.py
+++
@@ -1,9 +0,0 @@
-import pandas as pd
-import matplotlib.pyplot as plt
-
-graph = pd.DataFrame( {'Top Fans in Cherprang Fanpage': ['1 Week', '2 Week', '3 Week', '1 Month', '2 Month', '3 Month','4Month'], 'Person TopFans': ['177', '72', '106', '536', '55', '25', '21']})
-graph['Person TopFans'] = graph['Person TopFans'].astype(int)
-
-graph.plot(kind='bar', x='Top Fans in Cherprang Fanpage', y='Person TopFans')
-plt.xticks(rotation=10)
-plt.show()
