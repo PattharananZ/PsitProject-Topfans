c2d0276a5a725be1fd1f3d4821b713e2aa3d6b47
PattharananZ <42967288+PattharananZ@users.noreply.github.com>
2018-11-30 13:15:10 +0700

    update

--- Tero.py
+++ Tero.py
@@ -4,12 +4,12 @@ import pygal
 
 def graphy():
 	""" import data male/female from excel by csv file """
-	data = pd.read_csv("update.csv")
-	plot_g = pygal.Bar()
+	data = pd.read_csv("update1.csv")
+	plot_g = pygal.Pie()
 	plot_g.title = "Like&Share update"
 	plot_g.x_labels = data.DATE
 	plot_g.y_labels = map(int, range(1, 40000, 500))
 	plot_g.add("LIKE", data.LIKE)
 	plot_g.add("SHARE", data.SHARE)
-	plot_g.render_to_file("updatebar.svg")
+	plot_g.render_to_file("ike.svg")
 graphy()
